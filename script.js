ac8c25f8da616978022852388e883a1a2d230fde
ghuston <55936884+ghuston@users.noreply.github.com>
2023-11-01 21:35:00 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,58 @@
-// script.js
 document.addEventListener('DOMContentLoaded', function() {
-  createCalendar(document.getElementById('calendar'), new Date());
-
-  // Event delegation to handle click on calendar days
-  document.getElementById('calendar').addEventListener('click', function(e) {
-    if (e.target && e.target.classList.contains('day')) {
-      const time = prompt('Enter the time you started smoking (HH:MM)');
-      // Save the data and update the color
-      if (time) {
-        const day = e.target.getAttribute('data-date');
-        localStorage.setItem(day, time);
-        e.target.style.backgroundColor = getColorForTime(time);
-      }
-    }
-  });
+  const now = new Date();
+  const currentMonth = now.getMonth();
+  const currentYear = now.getFullYear();
+  buildCalendar(currentMonth, currentYear);
 });
 
-function createCalendar(container, date) {
-  // Code to create days and attach to container...
+function buildCalendar(month, year) {
+  const daysOfWeek = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+  const numOfDays = new Date(year, month + 1, 0).getDate();
+  const firstDay = new Date(year, month, 1).getDay();
+
+  let calendar = document.getElementById('calendar');
+  calendar.innerHTML = ''; // Clear the previous calendar
+
+  // Create the days of the week headers
+  daysOfWeek.forEach(day => {
+    let dayDiv = document.createElement('div');
+    dayDiv.className = 'day';
+    dayDiv.innerText = day;
+    calendar.appendChild(dayDiv);
+  });
+
+  // Add empty days for the first week if needed
+  for (let i = 0; i < firstDay; i++) {
+    let emptyDiv = document.createElement('div');
+    emptyDiv.className = 'day';
+    calendar.appendChild(emptyDiv);
+  }
+
+  // Add the days of the month
+  for (let i = 1; i <= numOfDays; i++) {
+    let dayDiv = document.createElement('div');
+    dayDiv.className = 'day';
+    let dateDiv = document.createElement('div');
+    dateDiv.innerText = i;
+    dateDiv.className = 'date';
+    dateDiv.style.backgroundColor = getHeatMapColor(); // Function to determine color
+    dayDiv.appendChild(dateDiv);
+    calendar.appendChild(dayDiv);
+  }
 }
 
-function getColorForTime(time) {
-  // Code to determine color based on time...
+function getHeatMapColor() {
+  // Placeholder for heatmap color, adjust as needed
+  let hours = Math.random() * 24; // Replace with actual hours
+  if (hours < 6) {
+    return 'red';
+  } else if (hours < 12) {
+    return 'green';
+  } else if (hours < 18) {
+    return 'orange';
+  } else {
+    return 'blue';
+  }
 }
+
+// Add more JavaScript to handle user input and local storage as needed
