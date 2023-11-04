1cee322755c2bbedba96a2688e60a4abe1f007c9
ghuston <55936884+ghuston@users.noreply.github.com>
2023-11-04 16:38:10 -0500

    Update script.js

--- script.js
+++ script.js
@@ -13,52 +13,69 @@ function buildCalendar(month, year) {
   let calendar = document.getElementById('calendar');
   calendar.innerHTML = ''; // Clear the previous calendar
 
+  // Create table for the calendar
+  let table = document.createElement('table');
+  let tableHead = document.createElement('thead');
+  let headerRow = document.createElement('tr');
+
   // Create the days of the week headers
   daysOfWeek.forEach(day => {
-    let dayDiv = document.createElement('div');
-    dayDiv.className = 'day';
-    dayDiv.innerText = day;
-    calendar.appendChild(dayDiv);
+    let dayHeader = document.createElement('th');
+    dayHeader.textContent = day;
+    headerRow.appendChild(dayHeader);
   });
+  tableHead.appendChild(headerRow);
+  table.appendChild(tableHead);
 
-  // Add empty days for the first week if needed
-  for (let i = 0; i < firstDay; i++) {
-    let emptyDiv = document.createElement('div');
-    emptyDiv.className = 'day';
-    calendar.appendChild(emptyDiv);
-  }
+  let tableBody = document.createElement('tbody');
+  let date = 1;
+  for (let i = 0; i < 6; i++) {
+    let row = document.createElement('tr');
 
-  // Add the days of the month
-  for (let i = 1; i <= numOfDays; i++) {
-    let dayDiv = document.createElement('div');
-    dayDiv.className = 'day';
-    let dateDiv = document.createElement('div');
-    dateDiv.innerText = i;
-    dateDiv.className = 'date';
-    dateDiv.style.backgroundColor = getHeatMapColor(); // Function to determine color
-    dayDiv.appendChild(dateDiv);
-    calendar.appendChild(dayDiv);
+    for (let j = 0; j < 7; j++) {
+      if (i === 0 && j < firstDay) {
+        let cell = document.createElement('td');
+        row.appendChild(cell);
+      } else if (date > numOfDays) {
+        break;
+      } else {
+        let cell = document.createElement('td');
+        let cellText = document.createElement('span');
+        cellText.textContent = date;
+        if (date === now.getDate() && year === now.getFullYear() && month === now.getMonth()) {
+          cell.classList.add('current-day');
+        }
+        // Color the cell based on smoking data
+        cell.style.backgroundColor = getHeatMapColor(date);
+        cell.appendChild(cellText);
+        row.appendChild(cell);
+        date++;
+      }
+    }
+
+    tableBody.appendChild(row);
   }
+  table.appendChild(tableBody);
+  calendar.appendChild(table);
 }
 
 function getHeatMapColor(day) {
   let savedData = localStorage.getItem('smokingData');
   let smokingData = savedData ? JSON.parse(savedData) : {};
-  let hours = smokingData[day]; // Retrieve the hours for the specific day
+  let hours = smokingData[`${day}`]; // Retrieve the hours for the specific day
 
-  // Assuming hours is in 24-hour format and a value of 0 means no smoking
+  // Color logic
   if (hours === 0) {
-    return 'blue';
+    return 'lightblue';
   } else if (hours <= 6) {
-    return 'red';
+    return 'lightgreen';
   } else if (hours <= 18) {
-    return 'green';
-  } else {
     return 'orange';
+  } else {
+    return 'red';
   }
 }
 
-// To save data you would have a function like:
 function saveSmokingTime(day, hours) {
   let smokingData = localStorage.getItem('smokingData');
   smokingData = smokingData ? JSON.parse(smokingData) : {};
@@ -66,5 +83,5 @@ function saveSmokingTime(day, hours) {
   localStorage.setItem('smokingData', JSON.stringify(smokingData));
 }
 
-
-// Add more JavaScript to handle user input and local storage as needed
+// Additional logic to handle user inputs, like clicking on a day to input smoking data,
+// can be added below. You can use event delegation on the table to handle this.
